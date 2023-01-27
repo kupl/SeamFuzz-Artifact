d9c70d0dde482023586950c0d3a695594a01bad0
audxo14 <audxo14@gmail.com>
2023-01-27 10:17:04 +0900

    update

--- scripts/get_implemented_target.sh
+++ scripts/get_implemented_target.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # a running script for SeamFuzz artifact accpeted in ICSE'23
 # As all experiments were evaluated on FuzzBench framework, 
-# this script will make a local experiment configuration for running FuzzBench framework in a local setting. 
-# TRIALS: the number of trials of the intended experiments. The default value is 20
-# TIME: the seconds for the running experiments. The default value is 86400, which is 24 hours
-# [EXP_PATH]: the path for storing the running data of FuzzBench. must be given as an absolute path. The default path is the working directory/data/
-# [REPORT]: the path for storing the report of FuzzBench. must be given as an absolute path. The default path is the working directory/report/
+# this script will use FuzzBench framework to obtain [FUZZER]-implemented programs and the corresponding [FUZZER] binary file. 
+# [FUZZBENCH] = an absolute path to FuzzBench framework (must be git cloned)
+# [FUZZER]: [FUZZER] binary file
+# [BENCHMARK]: [FUZZER]-implemented benchmark programs
 #
 # ./get_implemented_target.sh.sh [FUZZBENCH] [FUZZER] [BENCHMARK] 
 
@@ -20,6 +19,11 @@ CHECK=`docker images | grep 'builders' | grep $FUZZER | grep $BENCHMARK | grep -
 
 CHECK_CONTAINER=`docker ps -a | grep 'get_container'`
 
+# If no image have ever been built...
+if [ $CHECK == "" ]; then	
+	cd $FUZZBENCH && source .venv/bin/activate && make build-$FUZZER-$BENCHMARK && deactivate
+fi
+
 if [ ! "$CHECK_CONTAINER" == "" ]; then
 	echo "HELLO"
 	DELETE=`docker rm $(docker ps -a | grep 'get_container')`
@@ -31,5 +35,3 @@ CONTAINER=`docker create --name='get_container' $CHECK`
 mkdir -p $IMPLEMENT_PATH
 
 docker cp get_container:/out/ ./implement/$BENCHMARK
-
-
