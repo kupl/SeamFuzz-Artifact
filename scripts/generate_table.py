281278d086191d9bc2caffbda85ec4e9f7d1880d
audxo14 <audxo14@gmail.com>
2023-01-28 13:09:08 +0900

    update

--- scripts/generate_table.py
+++ scripts/generate_table.py
@@ -13,12 +13,194 @@ from matplotlib import pyplot as plt
 from matplotlib_venn import venn3
 import os
 
+def draw_table_total_result_with_ratio(col, result_path, total_cov_dict, total_crash_dict, total_ratio_cov_dict, total_ratio_crash_dict):
+    f = open(result_path + "/result_table.txt", 'a+')
+    f.write(f'{"Total":30s}|')
+    line_str="------------------------------" * col
+    table_line = line_str + "-" * len(fuzzers) + "\n"
+
+    i = 0
+    for fuzzer in fuzzers:
+        if i == 0:
+            f.write(f'{total_cov_dict[fuzzer]:>15d}' + f'{total_crash_dict[fuzzer]:>15d}|')
+            i += 1
+        else:
+            f.write(f'{total_cov_dict[fuzzer]:>15d}' + f'{total_crash_dict[fuzzer]:>15d}' + f'{total_ratio_cov_dict[fuzzer]:>15s}' + f'{total_ratio_crash_dict[fuzzer]:>15s}|')            
+
+    f.write("\n")        
+    f.write(table_line + "\n\n")
+
+    return
+    
+def draw_table_total_result_with_no_ratio(col, result_path, total_cov_dict, total_crash_dict):
+    f = open(result_path + "/result_table.txt", 'a+')
+    f.write(f'{"Total":30s}|')
+    line_str="------------------------------" * col
+    table_line = line_str + "-" * len(fuzzers) + "\n"
+
+    for fuzzer in fuzzers:
+        f.write(f'{total_cov_dict[fuzzer]:>15d}' + f'{total_crash_dict[fuzzer]:>15d}|')
+
+    f.write("\n")        
+    f.write(table_line + "\n\n")
+
+    return
+
+
+def draw_table_venn(result_path, fuzzers, benchmarks, unique_crashes_dict):
+    f = open(result_path + "/result_table.txt", 'a+')
+
+    # Generate Venn-diagram for the unique vulnerabilities found by each fuzzer.
+    tmp_crash_dict = dict()
+    crash_elements = []
+    fuzzer_elements = []
+    for fuzzer in fuzzers:
+        tmp_set = set()
+        for b in benchmarks:
+            tmp_set |= unique_crashes_dict[fuzzer][b]
+
+        tmp_crash_dict[fuzzer] = tmp_set
+        crash_elements.append(tmp_set)
+        fuzzer_elements.append(fuzzer)
+
+
+    v = venn3(crash_elements, tuple(fuzzer_elements))
+    #v=venn3([tmp_crash_dict['aflpp'], tmp_crash_dict['aflppmopt'], tmp_crash_dict['seam']], ('aflpp', 'aflppmopt', 'seam'))
+
+    plt.title("Unique Vulnerabilities")
+    plt.savefig(result_path + "/unique_vul_venn_diagram.png")
+
+    f.write(f'{"program":^30s}|' + f'{"# Unique Vulnerabilities":^30s}' + "\n")
+    f.write("-----------------------------------------------------------\n")
+    tmp_str = v.get_label_by_id('111').get_text()
+    f.write(f'{"AFL++ & AFL++_MOpt & SeamFuzz":^30s}|' + f'{tmp_str:^30s}' + "\n")
+    tmp_str = v.get_label_by_id('110').get_text()
+    f.write(f'{"Only AFL++ & AFL++_MOpt":^30s}|' + f'{tmp_str:^30s}' + "\n")
+    tmp_str = v.get_label_by_id('101').get_text()
+    f.write(f'{"Only AFL++ & SeamFuzz":^30s}|' + f'{tmp_str:^30s}' + "\n")
+    tmp_str = v.get_label_by_id('011').get_text()
+    f.write(f'{"Only AFL++_MOpt & SeamFuzz":^30s}|' + f'{tmp_str:^30s}' + "\n")
+    tmp_str = v.get_label_by_id('100').get_text()
+    f.write(f'{"Only AFL++":^30s}|' + f'{tmp_str:^30s}' + "\n")
+    tmp_str = v.get_label_by_id('010').get_text()
+    f.write(f'{"Only AFL++_MOpt":^30s}|' + f'{tmp_str:^30s}' + "\n")
+    tmp_str = v.get_label_by_id('001').get_text()
+    f.write(f'{"Only SeamFuzz":^30s}|' + f'{tmp_str:^30s}' + "\n")
+    f.write("-----------------------------------------------------------\n")
+
+    f.close()
+
+    return
+    
+def draw_table_with_no_ratio(col, result_path, exp_name, given_time, given_trials, benchmarks, fuzzers, mean_coverage_dict, mean_crash_dict):
+    f = open(result_path + "/result_table.txt", 'w')
+
+    # print the result table...
+    f.write("Experiment  : " + exp_name + "\n")
+    f.write("Total Time  : " + given_time + " seconds\n")
+    f.write("Total trials: " + given_trials + " trials\n")
+    line_str="------------------------------" * col
+    table_line = line_str  + "-" * len(fuzzers) + "\n"
+    f.write(table_line)
+    #f.write(f'{"program":^30s}|' + f'{"AFL++":^30s}|' + f'{"AFL++_MOpt":^60s}|' + f'{"SeamFuzz":^60s}|' + "\n")
+
+    i = 0
+    f.write(f'{"program":^30s}|')
+
+    for fuzz in fuzzers:
+        if i == 0:
+            f.write(f'{fuzz:^30s}|')
+            i+=1
+        else:
+            f.write(f'{fuzz:^30s}|')
+
+    f.write("\n")
+
+    f.write(f'{"":^30s}|')
+
+    for fuzzer in fuzzers:
+        f.write(f'{"Cover":^15s}' + f'{"Crashes":^15s}|')
+
+    f.write("\n")
+
+    f.write(table_line)
+
+    for b in benchmarks:
+        f.write(f'{b:30s}|')
+        
+        for fuzzer in fuzzers:
+            f.write(f'{mean_coverage_dict[fuzzer][b]:>15d}' + f'{mean_crash_dict[fuzzer][b]:>15d}|')
+
+
+        f.write("\n")
+
+    f.write(table_line)
+
+    f.close()
+
+
+def draw_table_with_ratio(col, result_path, exp_name, given_time, given_trials, benchmarks, fuzzers, mean_coverage_dict, mean_crash_dict, ratio_coverage_dict, ratio_crash_dict):
+    f = open(result_path + "/result_table.txt", 'w')
+
+    # print the result table...
+    f.write("Experiment  : " + exp_name + "\n")
+    f.write("Total Time  : " + given_time + " seconds\n")
+    f.write("Total trials: " + given_trials + " trials\n")
+    line_str="------------------------------" * col
+    table_line = line_str  + "-" * len(fuzzers) + "\n"
+    f.write(table_line)
+    #f.write(f'{"program":^30s}|' + f'{"AFL++":^30s}|' + f'{"AFL++_MOpt":^60s}|' + f'{"SeamFuzz":^60s}|' + "\n")
+
+    i = 0
+    f.write(f'{"program":^30s}|')
+
+    for fuzz in fuzzers:
+        if i == 0:
+            f.write(f'{fuzz:^30s}|')
+            i+=1
+        else:
+            f.write(f'{fuzz:^60s}|')
+
+    f.write("\n")
+
+    
+    i = 0
+    f.write(f'{"":^30s}|')
+    for fuzzer in fuzzers:
+        if i == 0:
+            f.write(f'{"Cover":^15s}' + f'{"Crashes":^15s}|')
+            i += 1
+        else:
+            f.write(f'{"Cover":^15s}' + f'{"Crashes":^15s}' + f'{"R_Cov":^15s}' + f'{"R_Crashes":^15s}|')
+
+    f.write("\n")
+
+    f.write(table_line)
+
+    for b in benchmarks:
+        f.write(f'{b:30s}|')
+        
+        for fuzzer in fuzzers:
+            if ("mopt" in fuzzer) or ("seam" in fuzzer):
+                f.write(f'{mean_coverage_dict[fuzzer][b]:>15d}' + f'{mean_crash_dict[fuzzer][b]:>15d}' + f'{ratio_coverage_dict[fuzzer][b]:>15s}' + f'{ratio_crash_dict[fuzzer][b]:>15s}|')
+            else:
+                f.write(f'{mean_coverage_dict[fuzzer][b]:>15d}' + f'{mean_crash_dict[fuzzer][b]:>15d}|')
+
+
+        f.write("\n")
+
+    f.write(table_line)
+
+    f.close()
+
+
 report_path = sys.argv[1]
 exp_path = sys.argv[2]
 given_time = sys.argv[3]
 given_trials = sys.argv[4]
 exp_name = sys.argv[5]
 result_path = sys.argv[6] + "/" + exp_name
+use_ratio = sys.argv[7]
 
 data = pd.read_csv(report_path + "/experimental/" + exp_name + "/data.csv.gz")
 df = pd.DataFrame(data, columns=data.keys())
@@ -80,7 +262,7 @@ for fuzzer in fuzzers:
         mean_crash_dict[fuzzer][b] = int(input_grouped.mean()[b])
         total_crash_dict[fuzzer] += int(input_grouped.mean()[b])
 
-f = open(result_path + "/result_table.txt", 'w')
+
 
 
 ratio_crash_dict = dict()
@@ -89,15 +271,20 @@ ratio_coverage_dict = dict()
 total_ratio_crash_dict = dict()
 total_ratio_cov_dict = dict()
 
-
+i = 0
+base_fuzz=""
 for fuzzer in fuzzers:
-    if ("mopt" in fuzzer) or ("seam" in fuzzer):
+    if i == 0:
+        base_fuzz=fuzzer
+        i+=1
+    else:
+    #if ("mopt" in fuzzer) or ("seam" in fuzzer):
         ratio_crash_dict[fuzzer] = dict()
         ratio_coverage_dict[fuzzer] = dict()
 
         for b in benchmarks:
-            if (mean_coverage_dict['aflpp'][b] != 0):
-                tmp_val = (float(mean_coverage_dict[fuzzer][b]) - float(mean_coverage_dict['aflpp'][b])) / float(mean_coverage_dict['aflpp'][b]) * 100
+            if (mean_coverage_dict[base_fuzz][b] != 0):
+                tmp_val = (float(mean_coverage_dict[fuzzer][b]) - float(mean_coverage_dict[base_fuzz][b])) / float(mean_coverage_dict[base_fuzz][b]) * 100
                 ratio_coverage_dict[fuzzer][b] = str(round(tmp_val, 1))
             elif (mean_coverage_dict[fuzzer][b] == 0):
                 ratio_coverage_dict[fuzzer][b] = "0%"
@@ -105,99 +292,37 @@ for fuzzer in fuzzers:
                 ratio_coverage_dict[fuzzer][b] = "NaN"
 
                 
-            if (mean_crash_dict['aflpp'][b] != 0):
-                tmp_val = (float(mean_crash_dict[fuzzer][b]) - float(mean_crash_dict['aflpp'][b])) / float(mean_crash_dict['aflpp'][b]) * 100
+            if (mean_crash_dict[base_fuzz][b] != 0):
+                tmp_val = (float(mean_crash_dict[fuzzer][b]) - float(mean_crash_dict[base_fuzz][b])) / float(mean_crash_dict[base_fuzz][b]) * 100
                 ratio_crash_dict[fuzzer][b] = str(round(tmp_val, 1))
             elif (mean_crash_dict[fuzzer][b] == 0):
                 ratio_crash_dict[fuzzer][b] = "0%"
             else:
                 ratio_crash_dict[fuzzer][b] = "NaN"
 
-        tmp_val = (float(total_cov_dict[fuzzer]) - float(total_cov_dict['aflpp'])) / float(total_cov_dict['aflpp']) * 100
+        tmp_val = (float(total_cov_dict[fuzzer]) - float(total_cov_dict[base_fuzz])) / float(total_cov_dict[base_fuzz]) * 100
         total_ratio_cov_dict[fuzzer] = str(round(tmp_val, 1))
         
-        if (total_crash_dict['aflpp'] != 0):
-            tmp_val = (float(total_crash_dict[fuzzer]) - float(total_crash_dict['aflpp'])) / float(total_crash_dict['aflpp']) * 100
+        if (total_crash_dict[base_fuzz] != 0):
+            tmp_val = (float(total_crash_dict[fuzzer]) - float(total_crash_dict[base_fuzz])) / float(total_crash_dict[base_fuzz]) * 100
             total_ratio_crash_dict[fuzzer] = str(round(tmp_val, 1))
         elif (mean_coverage_dict[fuzzer][b] == 0):
             total_ratio_crash_dict[fuzzer] = "0%"
         else:
             total_ratio_crash_dict[fuzzer] = "NaN"
 
+col = 1 + len(fuzzers)
+if use_ratio == "1":
+    col = col + len(ratio_crash_dict)
+    draw_table_with_ratio(col, result_path, exp_name, given_time, given_trials, benchmarks, fuzzers, mean_coverage_dict, mean_crash_dict, ratio_coverage_dict, ratio_crash_dict)
+    draw_table_total_result_with_ratio(col, result_path, total_cov_dict, total_crash_dict, total_ratio_cov_dict, total_ratio_crash_dict)
+else:
+    draw_table_with_no_ratio(col, result_path, exp_name, given_time, given_trials, benchmarks, fuzzers, mean_coverage_dict, mean_crash_dict)
+    draw_table_total_result_with_no_ratio(col, result_path, total_cov_dict, total_crash_dict)
 
-# print the result table...
-f.write("Experiment  : " + exp_name + "\n")
-f.write("Total Time  : " + given_time + " seconds\n")
-f.write("Total trials: " + given_trials + " trials\n")
-f.write("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
-f.write(f'{"program":^30s}|' + f'{"AFL++":^30s}|' + f'{"AFL++_MOpt":^60s}|' + f'{"SeamFuzz":^60s}|' + "\n")
-f.write(f'{"":^30s}|' + f'{"Cover":^15s}' + f'{"Crashes":^15s}|' + f'{"Cover":^15s}' + f'{"Crashes":^15s}' + f'{"R_Cov":^15s}' + f'{"R_Crashes":^15s}|' + f'{"Cover":^15s}' + f'{"Crashes":^15s}' + f'{"R_Cov":^15s}' + f'{"R_Crashes":^15s}|' + "\n")
-f.write("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
 
-for b in benchmarks:
-    f.write(f'{b:30s}|')
+draw_table_venn(result_path, fuzzers, benchmarks, unique_crashes_dict)
     
-    for fuzzer in fuzzers:
-        if ("mopt" in fuzzer) or ("seam" in fuzzer):
-            f.write(f'{mean_coverage_dict[fuzzer][b]:>15d}' + f'{mean_crash_dict[fuzzer][b]:>15d}' + f'{ratio_coverage_dict[fuzzer][b]:>15s}' + f'{ratio_crash_dict[fuzzer][b]:>15s}|')
-        else:
-            f.write(f'{mean_coverage_dict[fuzzer][b]:>15d}' + f'{mean_crash_dict[fuzzer][b]:>15d}|')
 
-
-    f.write("\n")
-
-f.write("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
-f.write(f'{"Total":30s}|')
-
-for fuzzer in fuzzers:
-    if ("mopt" in fuzzer) or ("seam" in fuzzer):
-        f.write(f'{total_cov_dict[fuzzer]:>15d}' + f'{total_crash_dict[fuzzer]:>15d}' + f'{total_ratio_cov_dict[fuzzer]:>15s}' + f'{total_ratio_crash_dict[fuzzer]:>15s}|')
-    else:
-        f.write(f'{total_cov_dict[fuzzer]:>15d}' + f'{total_crash_dict[fuzzer]:>15d}|')
-
-f.write("\n")        
-f.write("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n\n\n")
-
-
-
-# Generate Venn-diagram for the unique vulnerabilities found by each fuzzer.
-tmp_crash_dict = dict()
-crash_elements = []
-fuzzer_elements = []
-for fuzzer in fuzzers:
-    tmp_set = set()
-    for b in benchmarks:
-        tmp_set |= unique_crashes_dict[fuzzer][b]
-
-    tmp_crash_dict[fuzzer] = tmp_set
-    crash_elements.append(tmp_set)
-    fuzzer_elements.append(fuzzer)
-
-
-v = venn3(crash_elements, tuple(fuzzer_elements))
-#v=venn3([tmp_crash_dict['aflpp'], tmp_crash_dict['aflppmopt'], tmp_crash_dict['seam']], ('aflpp', 'aflppmopt', 'seam'))
-
-plt.title("Unique Vulnerabilities")
-plt.savefig(result_path + "/unique_vul_venn_diagram.png")
-
-f.write(f'{"program":^30s}|' + f'{"# Unique Vulnerabilities":^30s}' + "\n")
-f.write("-----------------------------------------------------------\n")
-tmp_str = v.get_label_by_id('111').get_text()
-f.write(f'{"AFL++ & AFL++_MOpt & SeamFuzz":^30s}|' + f'{tmp_str:^30s}' + "\n")
-tmp_str = v.get_label_by_id('110').get_text()
-f.write(f'{"Only AFL++ & AFL++_MOpt":^30s}|' + f'{tmp_str:^30s}' + "\n")
-tmp_str = v.get_label_by_id('101').get_text()
-f.write(f'{"Only AFL++ & SeamFuzz":^30s}|' + f'{tmp_str:^30s}' + "\n")
-tmp_str = v.get_label_by_id('011').get_text()
-f.write(f'{"Only AFL++_MOpt & SeamFuzz":^30s}|' + f'{tmp_str:^30s}' + "\n")
-tmp_str = v.get_label_by_id('100').get_text()
-f.write(f'{"Only AFL++":^30s}|' + f'{tmp_str:^30s}' + "\n")
-tmp_str = v.get_label_by_id('010').get_text()
-f.write(f'{"Only AFL++_MOpt":^30s}|' + f'{tmp_str:^30s}' + "\n")
-tmp_str = v.get_label_by_id('001').get_text()
-f.write(f'{"Only SeamFuzz":^30s}|' + f'{tmp_str:^30s}' + "\n")
-f.write("-----------------------------------------------------------\n")
-
-f.close()
 
 
