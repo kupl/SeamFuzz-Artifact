dcfbc62fd06f214dc11b40d5b07bba995dfe28cc
audxo14 <audxo14@gmail.com>
2023-01-28 10:21:22 +0900

    Update and rename get_implemented_target.sh to get_instrumented_target.sh

--- scripts/get_instrumented_target.sh
+++ scripts/get_instrumented_target.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # a running script for SeamFuzz artifact accpeted in ICSE'23
 # As all experiments were evaluated on FuzzBench framework, 
-# this script will use FuzzBench framework to obtain [FUZZER]-implemented programs and the corresponding [FUZZER] binary file. 
+# this script will use FuzzBench framework to obtain [FUZZER]-instrumented programs and the corresponding [FUZZER] binary file. 
 # [FUZZBENCH] = an absolute path to FuzzBench framework (must be git cloned)
 # [FUZZER]: [FUZZER] binary file
-# [BENCHMARK]: [FUZZER]-implemented benchmark programs
+# [BENCHMARK]: [FUZZER]-instrumented benchmark programs
 #
-# ./get_implemented_target.sh.sh [FUZZBENCH] [FUZZER] [BENCHMARK] 
+# ./get_instrumented_target.sh.sh [FUZZBENCH] [FUZZER] [BENCHMARK] 
 
 SHELL_PATH=`pwd -P`
 CONFIG="local-experiment-config.yaml"
-IMPLEMENT_PATH=$SHELL_PATH/implement
+INSTRUMENT_PATH=$SHELL_PATH/instrument
 FUZZBENCH=${1}
 FUZZER=${2}
 BENCHMARK=${3}
@@ -32,8 +32,8 @@ fi
 
 CONTAINER=`docker create --name='get_container' $CHECK`
 
-mkdir -p $IMPLEMENT_PATH
+mkdir -p $INSTRUMENT_PATH
 
-docker cp get_container:/out/ $IMPLEMENT_PATH/$BENCHMARK
+docker cp get_container:/out/ $INSTRUMENT_PATH/$BENCHMARK
 
 echo $DELETE
